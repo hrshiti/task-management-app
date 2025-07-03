0c885df85c09934dd615a53a442fe8c8699923b2
hrshiti <hritik45raghuwanshi@gmail.com>
2025-07-03 09:21:48 +0530

    new storage

--- server/routers/task-router.js
+++ server/routers/task-router.js
@@ -4,9 +4,13 @@ const express = require('express');
 
 const { createTask, getTasks, updateTask, deleteTask, getStudentTasks, submitTask, getSubmittedTasks, evaluateTask, addComment } = require('../controllers/task-controller');
 const authMiddleware = require('../middlewares/auth-middleware');
-const upload = require('../middlewares/uploadMiddleware');
+// const upload = require('../middlewares/uploadMiddleware');
 const roleMiddleware = require('../middlewares/roleMiddleware');
 const router = express.Router();
+const multer = require('multer');
+const { storage } = require('../utils/cloudinary');
+
+const upload = multer({ storage: storage });
 
 router.post('/create', authMiddleware,roleMiddleware('teacher','admin'), createTask);
 router.get('/gettask', authMiddleware, getTasks);
